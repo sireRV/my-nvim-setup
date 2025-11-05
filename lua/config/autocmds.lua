5041444b8befa369e75fbd98f5da09fed9730371
Sensei S <senseisingal@gmail.com>
2025-11-05 22:25:04 +1100

    some vim keymaps and lazydev install to get rid of vim warning

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,12 @@
+-- Highlight the yanked text for 200ms
+local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = highlight_yank_group,
+	pattern = "*",
+	callback = function()
+		vim.hl.on_yank({
+			higroup = "IncSearch",
+			timeout = 200,
+		})
+	end,
+})
